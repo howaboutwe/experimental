0e290c6c6f7434835ca44c34869e00c405d5cef4
George Ogata <george.ogata@gmail.com>
2013-03-25 15:07:28 -0400

    Unneeded assignment.

--- app/models/experimental/population/filter.rb
+++ app/models/experimental/population/filter.rb
@@ -2,7 +2,6 @@ module Experimental
   module Population
     module Filter
       def find_population(name)
-        p = nil
         klass = name.to_s.camelize
         if name && Experimental::Population.const_defined?(klass)
           p = Experimental::Population.const_get klass
