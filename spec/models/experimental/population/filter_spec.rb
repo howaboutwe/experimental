793d9eea0375b8fedfe38079ee86c7fe064b0028
George Ogata <george.ogata@gmail.com>
2013-03-25 16:01:36 -0400

    Support registering custom population filters outside the Experimental namespace.
    This also potentially changes the old behavior in that an invalid experiment
    name will return nil, rather than the default population filter.

--- spec/models/experimental/population/filter_spec.rb
+++ spec/models/experimental/population/filter_spec.rb
@@ -9,5 +9,23 @@ describe Experimental::Population::Filter do
     it "returns the default if nil is given" do
       Experimental::Experiment.find_population(nil).should == Experimental::Population::Default
     end
+
+    it "returns the default if a blank string is given" do
+      Experimental::Experiment.find_population(' ').should == Experimental::Population::Default
+    end
+
+    it "returns a custom population filter registered with the given name" do
+      filter_class = Class.new
+      Experimental::Experiment.register_population_filter(:custom, filter_class)
+      begin
+        Experimental::Experiment.find_population(:custom).should equal(filter_class)
+      ensure
+        Experimental::Experiment.reset_population_filters
+      end
+    end
+
+    it "returns nil if an unregistered name is given" do
+      Experimental::Experiment.find_population('unregistered').should be_nil
+    end
   end
 end
