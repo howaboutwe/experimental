53a586db7fb63b9e2c642d0a0ad5871d2a617701
George Ogata <george.ogata@gmail.com>
2014-06-13 13:34:48 -0400

    Trim down the dummy app.

--- spec/dummy/config/initializers/experimental.rb
+++
@@ -1,6 +0,0 @@
-#Load the population classes
-#Rails lazy loads and since they are never explicitly referenced, they don't get loaded
-#Deleting this file causes failures when running individual specs
-Dir[File.join(Rails.root, 'app/models/experiment/population/*.rb')].each do |f|
-  require f
-end
