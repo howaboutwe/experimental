6500263fdc68e1754f18cfc53479cd646f084e14
Bryan Woods <bryanwoods4e@gmail.com>
2014-06-16 16:19:26 -0400

    Minimizes complexity in experimental loader
    * Get some of that sweet sweet 4.0 Code Climate goodness

--- lib/experimental/loader.rb
+++ lib/experimental/loader.rb
@@ -12,32 +12,52 @@ module Experimental
       logger.info "Synchronizing experiments..."
 
       Experimental::Experiment.transaction do
-        active = Experimental.experiment_data.map do |name, attributes|
-          experiment = Experimental::Experiment.where(name: name).first_or_initialize
-
-          unstarted = attributes.delete('unstarted')
-          defaults = {'num_buckets' => nil, 'notes' => nil, 'population' => nil}
-          experiment.assign_attributes(defaults.merge(attributes))
-          if unstarted
-            experiment.start_date = nil
-          else
-            experiment.start_date ||= Time.current
-          end
-
-          logger.info "  * #{experiment.id ? 'updating' : 'creating'} #{name}"
-          experiment.tap(&:save!)
-        end
-
         scope = Experimental::Experiment.in_code
-        scope = scope.where('id NOT IN (?)', active.map(&:id)) unless active.empty?
-        scope.find_each do |experiment|
-          next if experiment.admin?
-          logger.info "  * removing #{experiment.name}"
-          experiment.remove
-        end
+        active_ids = updated_active_experiments.map(&:id)
+        active_ids.empty? or
+          scope = scope.where('id NOT IN (?)', active_ids)
+
+        remove_experiments(scope)
       end
 
       logger.info "Done."
+    end
+
+    private
+
+    def updated_active_experiments
+      Experimental.experiment_data.map do |name, attributes|
+        experiment = Experimental::Experiment.where(name: name).
+          first_or_initialize
+
+        nullify_attributes(experiment, attributes)
+
+        logger.info "  * #{experiment.id ? 'updating' : 'creating'} #{name}"
+
+        experiment.tap(&:save!)
+      end
+    end
+
+    def remove_experiments(scope)
+      scope.find_each do |experiment|
+        next if experiment.admin?
+        logger.info "  * removing #{experiment.name}"
+        experiment.remove
+      end
+    end
+
+    def nullify_attributes(experiment, attributes)
+      set_new_start_date(experiment, attributes)
+      defaults = {'num_buckets' => nil, 'notes' => nil, 'population' => nil}
+      experiment.assign_attributes(defaults.merge(attributes))
+    end
+
+    def set_new_start_date(experiment, attributes)
+      if (unstarted = attributes.delete('unstarted'))
+        experiment.start_date = nil
+      else
+        experiment.start_date ||= Time.current
+      end
     end
   end
 end
